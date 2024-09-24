71ed338d4e81b68af49a5e297edd46d98a8bb481
Ubuntu <ubuntu@ip-10-0-22-54.us-west-2.compute.internal>
2024-09-24 17:36:40 +0000

    Finished benchmarks on L40S

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+./utils/sync_data.sh
+./benchmark.sh 
+./preprocess.sh 
--- utils/sync_data.sh
+++ utils/sync_data.sh
@@ -6,7 +6,7 @@ DATA_DIR="/opt/dlami/nvme/data"
 mkdir -p ${DATA_DIR}
 
 # Upload 
-aws s3 sync ${DATA_DIR} s3://complete-genomics-benchmarking-data/
+# aws s3 sync ${DATA_DIR} s3://complete-genomics-benchmarking-data/
 
 # Download 
-# aws s3 sync s3://complete-genomics-benchmarking-data/ ${DATA_DIR}
+aws s3 sync s3://complete-genomics-benchmarking-data/ ${DATA_DIR}
