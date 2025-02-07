2cb6ef978219b0bfefc56c9bb8ea11b8bf72982c
Ubuntu <ubuntu@ip-10-0-22-54.us-west-2.compute.internal>
2025-02-07 16:07:27 +0000

    Fixing issue with downloading reference files. Added files to move index to nvme_dir

--- utils/move_data.sh
+++ utils/move_data.sh
@@ -15,14 +15,17 @@ mkdir -p ${NVME_DIR}/data/ref
 echo "Moving reference files"
 REF_DIR="/home/ubuntu/complete-genomics-benchmarks/data/ref"
 
-# rsync -ah --progress ${REF_DIR}/ucsc.* ${NVME_DIR}/data/ref
+rsync -ah --progress ${REF_DIR}/ucsc.* ${NVME_DIR}/data/ref
 rsync -ah --progress ${REF_DIR}/dbsnp_151.vcf.gz ${NVME_DIR}/data/ref
+rsync -ah --progress ${REF_DIR}/dbsnp_151.vcf.gz.tbi ${NVME_DIR}/data/ref
 rsync -ah --progress ${REF_DIR}/1000G_phase1.indels.hg19.sites.vcf.gz ${NVME_DIR}/data/ref
+rsync -ah --progress ${REF_DIR}/1000G_phase1.indels.hg19.sites.vcf.idx.gz ${NVME_DIR}/data/ref
 rsync -ah --progress ${REF_DIR}/Mills_and_1000G_gold_standard.indels.hg19.sites.vcf.gz ${NVME_DIR}/data/ref
+rsync -ah --progress ${REF_DIR}/Mills_and_1000G_gold_standard.indels.hg19.sites.vcf.idx.gz ${NVME_DIR}/data/ref
 
 # Move data files 
-# echo "Moving data files"
-# DATA_DIR="/home/ubuntu/complete-genomics-benchmarks/data"
+echo "Moving data files"
+DATA_DIR="/home/ubuntu/complete-genomics-benchmarks/data"
 
-# rsync -ah --progress ${DATA_DIR}/DL100002760_L01_read_1.fq.gz ${NVME_DIR}/data 
-# rsync -ah --progress ${DATA_DIR}/DL100002760_L01_read_2.fq.gz ${NVME_DIR}/data
+rsync -ah --progress ${DATA_DIR}/DL100002760_L01_read_1.fq.gz ${NVME_DIR}/data 
+rsync -ah --progress ${DATA_DIR}/DL100002760_L01_read_2.fq.gz ${NVME_DIR}/data
