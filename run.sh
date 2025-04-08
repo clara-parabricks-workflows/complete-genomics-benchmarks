d9dba0c6acc81c9187dd549ed88ea248cf5c9f4f
Gary Burnett <gburnett@nvidia.com>
2025-04-08 00:02:44 +0000

    Adding downsampling

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-./download.sh 
+# Download the data and reference files 
+# ./download.sh 
 
-./downsample.sh 
+# Downsample the T7 data from 46x coverage down to 30x 
+# ./downsample.sh E100030471QC960_L01_48_1.fq.gz 0.65
+# ./downsample.sh E100030471QC960_L01_48_2.fq.gz 0.65
