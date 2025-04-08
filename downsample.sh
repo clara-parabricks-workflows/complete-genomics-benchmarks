83f6d9488007242b3f7067efd90975857369944b
Gary Burnett <gburnett@nvidia.com>
2025-04-08 01:06:59 +0000

    Keeping file after zip

--- downsample.sh
+++ downsample.sh
@@ -6,4 +6,4 @@ DOWNSAMPLE_FRAC="$2"
 # Pigz is like gzip but multi-threaded 
 pigz -d data/"${SAMPLE_NAME}.fq.gz"
 seqtk sample -s100 "data/${SAMPLE_NAME}.fq" ${DOWNSAMPLE_FRAC} > data/${SAMPLE_NAME}.30x.fq
-pigz data/${SAMPLE_NAME}.30x.fq
+pigz -k data/${SAMPLE_NAME}.30x.fq
