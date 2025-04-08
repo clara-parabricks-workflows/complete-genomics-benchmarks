cc76669f971263641914805add826db3ea8bf244
Gary Burnett <33267967+gburnett-nvidia@users.noreply.github.com>
2025-04-08 15:48:58 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download the data and reference files 
-# ./download.sh 
+./download.sh 
 
 # Downsample the T7 data from 46x coverage down to 30x 
 ./downsample.sh E100030471QC960_L01_48_1.fq.gz 0.65
