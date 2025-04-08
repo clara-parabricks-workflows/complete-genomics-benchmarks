099582bfce354c746063a51ae1b8f3aa9655319a
Gary Burnett <gburnett@nvidia.com>
2025-04-08 01:05:15 +0000

    Adding benchmarking

--- run.sh
+++ run.sh
@@ -4,5 +4,8 @@
 # ./download.sh 
 
 # Downsample the T7 data from 46x coverage down to 30x 
-# ./downsample.sh E100030471QC960_L01_48_1.fq.gz 0.65
-# ./downsample.sh E100030471QC960_L01_48_2.fq.gz 0.65
+./downsample.sh E100030471QC960_L01_48_1.fq.gz 0.65
+./downsample.sh E100030471QC960_L01_48_2.fq.gz 0.65
+
+# Run benchmarks 
+# ./benchmark.sh L4
