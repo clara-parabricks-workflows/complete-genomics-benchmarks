4a4db67cbcb54113d72aa72989cd48b49d095669
Gary Burnett <gburnett@nvidia.com>
2025-04-08 01:33:51 +0000

    Removing confusing nvme_drive references everywhere

--- benchmark.sh
+++ benchmark.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Check that the environment variable is set 
-if [[ ! -v NVME_DIR ]]; then
-    echo "NVME_DIR environment variable is not set. Exiting."
-    echo "export NVME_DIR=/opt/dlami/nvme"
-    exit
-fi
-
 # Check that the hardware is set 
 if [[ -z $1 ]]; then
-    echo "Missing hardware setting. Exiting."
-    echo "./benchmark.sh L4"
+    echo "Missing data directory. Exiting."
+    echo "./benchmark.sh /opt/dlami/nvme L4"
     exit
 fi
 
-HARDWARE="$1"
+if [[ -z $2 ]]; then
+    echo "Missing hardware setting. Exiting."
+    echo "./benchmark.sh /opt/dlami/nvme L4"
+    exit
+fi
+
+DATA_DIR="$1"
+HARDWARE="$2"
 BENCHMARK_PATH="./benchmarks/${HARDWARE}"
 
-mkdir -p ${NVME_DIR}/tmp
-mkdir -p ${NVME_DIR}/data/logs
-mkdir -p ${NVME_DIR}/data/outdir
+mkdir -p ${DATA_DIR}/tmp
+mkdir -p ${DATA_DIR}/data/logs
+mkdir -p ${DATA_DIR}/data/outdir
 
 # Define samples 
 declare -a T1Plus_samples=(30x_DL100002760_L01_NA12878_1.fq.gz 30x_DL100002760_L01_NA12878_2.fq.gz)
@@ -27,7 +27,8 @@ declare -a T7_samples=(E100030471QC960_L01_48_1.30x.fq.gz E100030471QC960_L01_48
 declare -a G400_samples=(G400_PE150_NA12878_WGS_V300046476_L01_1.fq.gz G400_PE150_NA12878_WGS_V300046476_L01_2.fq.gz)
 
 # Note: Array contents must match names declared above 
-declare -a all_samples=("T1Plus_samples" "T7_samples" "G400_samples")
+# declare -a all_samples=("T1Plus_samples" "T7_samples" "G400_samples")
+declare -a all_samples=("T7_samples" "G400_samples")
 
 # Run benchmarks on every set of samples 
 for s in "${all_samples[@]}"; do
@@ -36,11 +37,11 @@ for s in "${all_samples[@]}"; do
     declare -n sample="$s"
 
     # Run germline benchmark 
-    ${BENCHMARK_PATH}/germline.sh ${sample[0]} ${sample[1]}
+    ${BENCHMARK_PATH}/germline.sh $1 ${sample[0]} ${sample[1]}
    
     # Run deepvariant benchmark 
     BAM="$(basename -s .fq.gz ${sample[0]}).bam"
-    ${BENCHMARK_PATH}/deepvariant.sh ${BAM}
+    ${BENCHMARK_PATH}/deepvariant.sh $1 ${BAM}
 
 done
 
--- run.sh
+++ run.sh
@@ -7,5 +7,12 @@
 ./downsample.sh E100030471QC960_L01_48_1.fq.gz 0.65
 ./downsample.sh E100030471QC960_L01_48_2.fq.gz 0.65
 
-# Run benchmarks 
-# ./benchmark.sh L4
+# Move data to NVME
+NVME_DIR=/opt/dlami/nvme
+rsync -ah --progress data $NVME_DIR
+
+# Run benchmarks
+./benchmark.sh $NVME_DIR L4
+
+# Move data back to home dir 
+rsync -ah --progress $NVME_DIR/data .
